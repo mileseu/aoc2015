04753193340dcf80c07c9cd12c009275a4aa5062
miles <mileseu1@gmail.com>
2022-05-07 23:05:33 +0100

    Day 4 Completed

--- day4.py
+++ day4.py
@@ -17,13 +17,4 @@ def hash_function():
     print(f"Hex value: {hex_value}, number: {start}")
     return 
 
-#wanted to use recursion but it is limited by memory (1000 recursions)
-''''    if str(hex_value[:5]) == "00000":
-        print(hex_value)
-        return hex_value
-    else:
-        number += 1
-        print(number)
-        hash_function(number)'''
-
 hash_function()
